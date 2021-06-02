daecafdfed1389e67ff9971469516cfaa23507ab
Bodrul Amin <bodrulaminiu@gmail.com>
2021-06-02 12:04:27 +0600

    msg

--- SQL-Commands/hr-all-statement.sql
+++ SQL-Commands/hr-all-statement.sql
@@ -67,8 +67,12 @@ select first_name, salary , hire_date
     from employees
     where hire_date > TO_DATE('01-JAN-2004');
 
-select first_name, salary , hire_date , round(((sysdate-hire_date)/365),0) as "Year"
-    from employees ;
+select first_name, salary , hire_date , round(((sysdate-hire_date)/365),0) as "Working YEAR"
+    from  employees;
+
+select first_name, salary , hire_date , round(((sysdate-hire_date)/365),0) as "Year>15"
+    from  employees 
+    where round(((sysdate-hire_date)/365),0)>15;
     
     
 
