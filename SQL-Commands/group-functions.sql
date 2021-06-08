e2cee27c4bd86ded533cca0c3bc65cb2f22472b3
Bodrul Amin <bodrulaminiu@gmail.com>
2021-06-08 13:16:28 +0600

    added some comn

--- SQL-Commands/group-functions.sql
+++ SQL-Commands/group-functions.sql
@@ -1,3 +1,24 @@
+
+--  Avarage salary of all employees
 select count(*) employees, avg(salary) salary from employees;
 
+-- find out distint departments of employees
 select count(DISTINCT(DEPARTMENT_ID)) from EMPLOYEEs;
+
+
+SELECT DEPARTMENT_ID, AVG(salary)
+    from EMPLOYEES
+    group by DEPARTMENT_ID;
+
+ 
+SELECT DEPARTMENT_ID, JOB_ID, sum(SALARY)
+    FROM EMPLOYEES
+    GROUP by DEPARTMENT_ID, JOB_ID
+    ORDER BY JOB_ID;
+
+
+SELECT DEPARTMENT_ID, avg(SALARY)
+    FROM EMPLOYEES
+    GROUP BY DEPARTMENT_ID
+    HAVING max(SALARY) > 10000;
+
