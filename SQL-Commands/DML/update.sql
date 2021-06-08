c975791a858646804c49f2d640f3a88e00c91cd4
Bodrul Amin <bodrulaminiu@gmail.com>
2021-06-08 12:22:08 +0600

    s

--- SQL-Commands/DML/update.sql
+++
@@ -1,8 +0,0 @@
-
-SELECT * from students;
--------update
-update students
-	set id=1254, name='bodrul s';
-    WHERE id=1254;
-
-SELECT * from students;
---
+++ SQL-Commands/group-functions.sql
@@ -0,0 +1,3 @@
+select count(*) employees, avg(salary) salary from employees;
+
+select count(DISTINCT(DEPARTMENT_ID)) from EMPLOYEEs;
