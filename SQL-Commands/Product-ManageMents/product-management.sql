6c091ff508bbe033ca847f15b94619c451a6a3dd
Bodrul Amin <bodrulaminiu@gmail.com>
2021-06-09 13:02:55 +0600

    added f

---
+++ SQL-Commands/Product-ManageMents/product-management.sql
@@ -0,0 +1,10 @@
+catagory, id, NAME
+product id,name, catagory id,  price
+invoice inv id, prod id, customar name, customar add, sell price, totalprice .discount 
+TRANSACTION id, name, ref id = invoice id,
+
+
+25 product
+10 sell
+invoice 5
+trancsactio 30 
--- SQL-Commands/group-functions.sql
+++ SQL-Commands/group-functions.sql
@@ -5,18 +5,18 @@ select count(*) employees, avg(salary) salary from employees;
 -- find out distint departments of employees
 select count(DISTINCT(DEPARTMENT_ID)) from EMPLOYEEs;
 
-
+-- avarage salary department wise
 SELECT DEPARTMENT_ID, AVG(salary)
     from EMPLOYEES
     group by DEPARTMENT_ID;
 
- 
+ -- dept wise salary order by job_i 
 SELECT DEPARTMENT_ID, JOB_ID, sum(SALARY)
     FROM EMPLOYEES
     GROUP by DEPARTMENT_ID, JOB_ID
     ORDER BY JOB_ID;
 
-
+-- avarage salary department wise greater than 1000;
 SELECT DEPARTMENT_ID, avg(SALARY)
     FROM EMPLOYEES
     GROUP BY DEPARTMENT_ID
