ac8ba99fa6b6caea8c4335d8ae38a93ed74f31f9
Bodrul Amin <bodrulaminiu@gmail.com>
2021-06-17 11:04:36 +0600

    m

--- Relation/student-college-bill.sql
+++ Relation/student-college-bill.sql
@@ -1,6 +1,42 @@
 
+select * from user_tables;
+
+
+drop table bill;
+drop table student;
+drop table college;
+
+
+
+CREATE table college (
+    id number(10) primary key,
+    name VARCHAR2(20),
+    ADDRESS VARCHAR2(20)
+);
 
 
 CREATE table student(
-    roll NUMBER(10)
-)
+    id NUMBER(10) primary key,
+    name VARCHAR2(20),
+    email VARCHAR2(20) UNIQUE,
+    college_id NUMBER(10),
+    constraint st_collid_fk foreign key(college_id) REFERENCES college(id)
+);
+
+
+
+CREATE table bill(
+    id NUMBER(10) PRIMARY key,
+    payment_type VARCHAR2(10),
+    fee VARCHAR2(10),
+    student_id NUMBER(10),
+    college_id NUMBER(10),
+    constraint bill_stid_fk foreign key(student_id) REFERENCES student(id),
+    constraint bill_collid_fk foreign key(college_id) REFERENCES college(id)
+    
+);
+
+
+desc college;
+desc student;
+desc bill;
